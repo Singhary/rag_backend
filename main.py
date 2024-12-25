7a4285ec9348747a3710776870df2bbdb0cadf12
Singhary <singhary449@gmail.com>
2024-12-25 17:19:40 +0530

    .

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import logging
 import os
 import shutil
 import uuid
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 from chroma_utils import delete_doc_from_chroma, index_document_to_chroma
